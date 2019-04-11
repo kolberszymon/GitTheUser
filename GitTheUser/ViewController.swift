7cbb38092810260aa7daca67dab9032d0acf521b
Szymon Kolber <kolberszymon@gmail.com>
2019-04-11 17:18:29 +0200

    Initial Commit

---
+++ GitTheUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTheUser
+//
+//  Created by Dominik Kolber on 4/11/19.
+//  Copyright © 2019 Szymon Kolber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
